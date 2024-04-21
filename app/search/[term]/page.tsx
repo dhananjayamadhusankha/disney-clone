e6af8f46fc5a7210d5046c7ed38d53ea1555f7de
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2024-04-21 22:02:24 +0530

    front page is completed

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,3 +1,5 @@
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getPopularMovies, getSearchMovies } from "@/lib/getMovies";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -6,11 +8,25 @@ type Props = {
   };
 };
 
-function SearchPage({ params: { term } }: Props) {
-  if(!term) return notFound();
+async function SearchPage({ params: { term } }: Props) {
+  if (!term) return notFound();
 
   const termToUse = decodeURI(term);
-  return <div>SearchPage {termToUse}</div>;
+
+  // API call to get the Searched Movies
+  const movies = await getSearchMovies(termToUse);
+
+  // API call to get the Popular Movies
+  const popularMovies = await getPopularMovies();
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        <h1 className="font-bold text-6xl md:text-7xl px-10">Results for {termToUse}</h1>
+        <MoviesCarousel movies={movies} title="Movies" isVertical />
+        <MoviesCarousel movies={popularMovies} title="You may also like" />
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
